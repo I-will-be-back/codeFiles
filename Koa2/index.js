52c37c8fd4933ac5d6ad31e9ecfc59d9a6420336
weichao <1505013204@qq.com>
2019-04-10 10:42:26 +0800

    updata[2019-4-10]

--- Koa2/index.js
+++ Koa2/index.js
@@ -1,51 +1,24 @@
-const koa = require('koa');
-const app = new koa();
+const Koa = require('koa');
+const app = new Koa();
+
 app.use(async(ctx) => {
-  const url = ctx.url;
-  const method = ctx.method;
-  if(url === '/' && method === 'GET') {
-    ctx.body = `
-    <form method='POST' action='/'>
-      <p>username</p><input name='username'>
-      <p>iphone</p><input name='iphone'>
-      <p>email</p><input name='email'>
-      <button type='submit'>submit</button>
-    </form>`;
-  } else if(url === '/' && method === 'POST') {
-    let postData = await parPostData(ctx);
-    ctx.body = postData;
+  if(ctx.url === '/index') {
+    ctx.cookies.set(
+      'name', 'carryChao', {
+        domain:'127.0.0.1',
+        //path:'/index',
+        maxAge:1000*60*60
+      });
+    ctx.body = 'dsad';
   } else {
-    ctx.body = `<h1>404!</h1>`;
-  }
-});
-app.listen(3000, () => {
-  console.log('server 3000 is running!');
-});
-
-function parPostData (ctx) {
-  return new Promise((resolve,reject) => {
-    try{
-      let postData = '';
-      ctx.req.addListener('data', (data) => {
-        postData += data;
-      });
-      ctx.req.on('end', () => {
-        postData = parseQueryStr(postData);
-        resolve(postData);
-      });
-    } catch (error) {
-      reject(error);
+    if(ctx.cookies.get('name')) {
+      ctx.body = ctx.cookies.get('name');
+    } else {
+      ctx.body = 'cookies is none';
     }
-  });
-}
-
-function parseQueryStr (parseStr) {
-  let json = {};
-  let parseList = parseStr.split('&');
-  for( let item of parseList) {
-    let data = item.split('=');
-    json[data[0]] = data[1];
   }
-  return json;
-  //console.log(json);
-}
+});
+
+app.listen(3000, () => {
+  console.log('server is running');
+});
