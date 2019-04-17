eb637802f152f5b355bbbc38f6a5954903a34809
weichao <1505013204@qq.com>
2019-04-17 15:54:47 +0800

    updata[2019-4-17]

--- mongo/test.js
+++ mongo/test.js
@@ -1,4 +1,8 @@
-let json = {'name':'smallPangChao','date':new Date()};
-let db = connect('log');
-db.login.insert(json);
-print('json is inserted');
+var json = {
+  findAndModify:'person',
+  query:{name:'xiaopangchao'},
+  update:{$set:{email:1505013204,time:new Date()}},
+  new:true
+}
+var result = db.runCommand(json);
+printjson(result);
