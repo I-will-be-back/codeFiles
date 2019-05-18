653e079698b9c01f7287f757fbb1c5ed4ea23b54
weichao <1505013204@qq.com>
2019-05-18 16:24:42 +0800

    updata[2019-5-18]

---
+++ webpack/babel/src/index.js
@@ -0,0 +1,8 @@
+const a = 1, b = 2, c = a + b;
+console.log(c);
+
+// [1, 2, 3].map(item => `${item}item`);
+
+// // es6的大部分功能, 其实es5也是能实现的 只不过更优雅
+// let xlz = {name: '范玲子', hometown: '吉林'}
+// xlz = {...xlz, "company": '新浪'};
