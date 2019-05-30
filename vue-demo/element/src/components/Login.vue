0ea0775cec8a8a56baf61e6b2871dd7fa616cf81
weichao <1505013204@qq.com>
2019-05-30 22:01:45 +0800

    updata[2019-5-30]

--- vue-demo/element/src/components/Login.vue
+++ vue-demo/element/src/components/Login.vue
@@ -28,7 +28,12 @@ export default {
     login () {
       if (this.user && this.password) {
         localStorage.setItem('user', this.user)
-        localStorage.setItem('password', this.password)
+        // 字符串
+        // this.$router.push('/main')
+        // 对象
+        // this.$router.push({path: '/main'})
+        // 编程式跳转
+        this.$router.push({name: 'Main', params: {userId: 1}})
       }
     }
   }
